a805de2bb705bb5bf2ea3dcb65a6a7667d03f54c
Joe Shoulak <joepshoulak@me.com>
2024-10-07 12:50:40 -0500

    the earliest stages of our scenes are forming

--- scene.py
+++ scene.py
@@ -1,14 +1,44 @@
-# from manim import *
-from manim import Scene, Circle, Square, RIGHT, TAU, PINK, Create, Transform, FadeOut
+from manim import *
 
-class SquareToCircle(Scene):
+def fibLines(num):
+    a, b = 0, 1
+    i = 0
+
+    while i < num:
+        yield f"{a}+{b}={a+b}"
+        a, b = b, a+b
+        i += 1
+
+class Fibonacci(Scene):
     def construct(self):
-        circle = Circle()
-        square = Square()
-        square.flip(RIGHT)
-        square.rotate(-3 * TAU / 8)
-        circle.set_fill(PINK, opacity=0.5)
+        title = Text("Fibonacci Numbers", font_size=89).shift(UP * 3)
+        definition = MathTex(r"f_{0} = 0, \ f_{1} = 1, \ f_{n}=f_{n-1}+f{n-2}", font_size=55).shift(UP * 2)
+        self.add(title)
+        self.add(definition)
 
-        self.play(Create(square))
-        self.play(Transform(square, circle))
-        self.play(FadeOut(square))
+        hD, vD = 0.75, 0.75
+        hOff, vOff = -3, -1.5
+
+        lines = []
+        for i, line in enumerate(fibLines(6)):
+            lines += Tex(line, font_size=55).shift(RIGHT * hD * (i+hOff)).shift(DOWN * vD * (i+vOff))
+
+        for line in lines:
+            self.play(Create(line))
+
+class Pisano(Scene):
+    def construct(self):
+        title = Text("Pisano Arrays", font_size=89).shift(UP * 3)
+        self.add(title)
+
+        pArr = Matrix([
+            [0, 5, 5, 0, 5, 5, 0, 5, 5, 0, 5, 5],
+            [1, 8, 9, 7, 6, 3, 9, 2, 1, 3, 4, 7],
+            [1, 3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2],
+            [2, 1, 3, 4, 7, 1, 8, 9, 7, 6, 3, 9],
+            [3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2, 1]])
+
+        pArr.scale(0.5)
+
+        self.play(Create(pArr))
+
