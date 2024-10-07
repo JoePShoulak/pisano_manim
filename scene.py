669291639dbf9b2e0e2bf075bd899a4dd1149a35
Joe Shoulak <joepshoulak@me.com>
2024-10-07 18:59:58 -0500

    more terms

--- scene.py
+++ scene.py
@@ -1,8 +1,8 @@
 from manim import *
 import math
 
-hD, vD = 0.75, 0.75
-hOff, vOff = -3, -1.5
+hD, vD = 0.5, 0.5
+hOff, vOff = -4, -3.5
 
 def fibLines(num):
     a, b = 0, 1
@@ -13,23 +13,27 @@ def fibLines(num):
         a, b = b, a+b
         i += 1
 
+bigFont = 55
+smallFont = 34
+
 def alignLines(eq, i):
     return eq.shift(RIGHT * hD * (i+hOff)).shift(DOWN * vD * (i+vOff))
 
 class Fibonacci(Scene):
     def construct(self):
         # Start with title already on screen, and draw the defintition of the Fibonacci numbers
-        title = Text("Fibonacci Numbers", font_size=89).to_edge(UP)
-        definition = MathTex(r"f_{0} = 0, \ f_{1} = 1, \ f_{n}=f_{n-1}+f_{n-2}", font_size=55).shift(UP * 2)
-        self.add(title)
+        title = Text("Fibonacci Numbers", font_size=bigFont).to_edge(UP)
+        definition = MathTex(r"f_{0} = 0, \ f_{1} = 1, \ f_{n}=f_{n-1}+f_{n-2}", font_size=smallFont).next_to(title, DOWN)
+        self.wait()
+        self.play(Write(title))
         self.play(Write(definition))
 
         # Setup all our equations to be drawn
         eqs = []
         reducedEqs = []
-        for i, eq in enumerate(fibLines(6)):
-            eqs += alignLines(Tex(eq, font_size=55), i)
-            reducedEqs += alignLines(Tex(eq.split('=')[1]), i)
+        for i, eq in enumerate(fibLines(9)):
+            eqs += alignLines(Tex(eq, font_size=smallFont), i)
+            reducedEqs += alignLines(Tex(eq.split('=')[1], font_size=smallFont), i)
 
         # Draw all the equations on the screen to begin our Fibonacci introduction
         for eq in eqs:
@@ -44,9 +48,9 @@ class Fibonacci(Scene):
         # align all our staggered numbers into one clean row
         alignAnim = list(map(lambda i: eqs[i+1].animate.align_to(eqs[0], UP), range(len(eqs)-1)))
         # add the numbers we lopped off the left, and an elipsis on the right
-        p1 = Tex("1", font_size=55).next_to(eqs[0], LEFT, buff=hD*2/3) # buff is not correct
-        p0 = Tex("0", font_size=55).next_to(p1, LEFT, buff=hD*2/3) # buff is not correct
-        cdots = Tex(r"\ldots", font_size=55).next_to(eqs[-1], RIGHT, buff=hD*2/3).align_to(p0, UP).shift(DOWN * 0.25) # This is also not correctly aligned
+        p1 = Tex("1", font_size=smallFont).next_to(eqs[0], LEFT, buff=hD*2/3) # buff is not correct
+        p0 = Tex("0", font_size=smallFont).next_to(p1, LEFT, buff=hD*2/3) # buff is not correct
+        cdots = Tex(r"\ldots", font_size=smallFont).next_to(eqs[-1], RIGHT, buff=hD*2/3).align_to(p0, UP).shift(DOWN * 0.25) # This is also not correctly aligned
         eqs = [p0, p1] + eqs
         self.play(
             AnimationGroup(
@@ -58,13 +62,13 @@ class Fibonacci(Scene):
 
         # Begin to show all the numbers mod 10
         modEqs = []
-        arrow = MathTex(r"\downarrow", font_size=55).next_to(eqs[0], DOWN)
+        arrow = MathTex(r"\downarrow", font_size=smallFont).next_to(eqs[0], DOWN)
 
         # Do this bit by bit
         for i, eq in enumerate(eqs):
             # Make our new equations
             s = eq.get_tex_string()
-            newEq = MathTex(int(s.split('=')[1] if s.find('=') != -1 else s)% 10, font_size=55)
+            newEq = MathTex(int(s.split('=')[1] if s.find('=') != -1 else s)% 10, font_size=smallFont)
             if i == 0:
                 newEq.next_to(arrow, DOWN)
             else:
@@ -81,7 +85,7 @@ class Fibonacci(Scene):
             )
 
         # Draw the dots and fade out the arrow to leave everything in a nice state for now
-        mdots = Tex(r"\ldots", font_size=55).next_to(modEqs[-1], RIGHT, buff=hD*2/3) # This is also not correctly aligned
+        mdots = Tex(r"\ldots", font_size=smallFont).next_to(modEqs[-1], RIGHT, buff=hD*2/3) # This is also not correctly aligned
         self.play(                
             AnimationGroup(
                     FadeOut(arrow),
@@ -93,7 +97,7 @@ class Fibonacci(Scene):
 
 class Pisano(Scene):
     def construct(self):
-        title = Text("Pisano Arrays", font_size=89).shift(UP * 3)
+        title = Text("Pisano Arrays", font_size=bigFont).shift(UP * 3)
         self.add(title)
 
         pArr = Matrix([
@@ -110,7 +114,7 @@ class Pisano(Scene):
 
 class Test(Scene):
     def construct(self):
-        title = Text("Pisano Arrays", font_size=89).to_edge(UP, buff=0.5)
+        title = Text("Pisano Arrays", font_size=bigFont).to_edge(UP, buff=0.5)
 
         self.play(Write(title, run_time=2))
         self.wait()
