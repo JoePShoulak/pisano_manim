7beb59038bb72923ecb8768f8a17e19d022ad61d
Joe Shoulak <joepshoulak@me.com>
2024-10-07 19:58:44 -0500

    getting closer to making the grid

--- scene.py
+++ scene.py
@@ -1,8 +1,8 @@
 from manim import *
 import math
 
-hD, vD = 0.5, 0.5
-hOff, vOff = -4, -3.5
+hD, vD = 0.75, 0.75
+hOff, vOff = -4, -1.5
 
 def fibLines(num):
     a, b = 0, 1
@@ -13,8 +13,8 @@ def fibLines(num):
         a, b = b, a+b
         i += 1
 
-bigFont = 55
-smallFont = 34
+bigFont = 89
+smallFont = 55
 
 def alignLines(eq, i):
     return eq.shift(RIGHT * hD * (i+hOff)).shift(DOWN * vD * (i+vOff))
@@ -31,7 +31,7 @@ class Fibonacci(Scene):
         # Setup all our equations to be drawn
         eqs = []
         reducedEqs = []
-        for i, eq in enumerate(fibLines(9)):
+        for i, eq in enumerate(fibLines(5)):
             eqs += alignLines(Tex(eq, font_size=smallFont), i)
             reducedEqs += alignLines(Tex(eq.split('=')[1], font_size=smallFont), i)
 
@@ -50,12 +50,16 @@ class Fibonacci(Scene):
         # add the numbers we lopped off the left, and an elipsis on the right
         p1 = Tex("1", font_size=smallFont).next_to(eqs[0], LEFT, buff=hD*2/3) # buff is not correct
         p0 = Tex("0", font_size=smallFont).next_to(p1, LEFT, buff=hD*2/3) # buff is not correct
-        cdots = Tex(r"\ldots", font_size=smallFont).next_to(eqs[-1], RIGHT, buff=hD*2/3).align_to(p0, UP).shift(DOWN * 0.25) # This is also not correctly aligned
-        eqs = [p0, p1] + eqs
+        n1 = Tex("13", font_size=smallFont).next_to(eqs[-1], RIGHT, buff=hD*2/3).align_to(p0, UP)
+        n2 = Tex("21", font_size=smallFont).next_to(n1, RIGHT, buff=hD*2/3).align_to(p0, UP)
+        n3 = Tex("34", font_size=smallFont).next_to(n2, RIGHT, buff=hD*2/3).align_to(p0, UP)
+        n4 = Tex("55", font_size=smallFont).next_to(n3, RIGHT, buff=hD*2/3).align_to(p0, UP)
+        cdots = Tex(r"\ldots", font_size=smallFont).next_to(n4, RIGHT, buff=hD*2/3).align_to(p0, UP).shift(DOWN * 0.25) # This is also not correctly aligned
+        eqs = [p0, p1] + eqs + [n1, n2, n3, n4]
         self.play(
             AnimationGroup(
                 AnimationGroup(*alignAnim),
-                AnimationGroup(Write(p0), Write(p1), Write(cdots)),
+                AnimationGroup(Write(p0), Write(p1), Write(n1), Write(n2), Write(n3), Write(n4), Write(cdots)),
                 lag_ratio=0.5
             )
         )
@@ -95,6 +99,20 @@ class Fibonacci(Scene):
 
         self.wait()
 
+        # Psuedo-scene change
+        newTitle = Text("Pisano Arrays", font_size=bigFont).to_edge(UP)
+        clearAnim = [FadeOut(definition), FadeOut(cdots), Transform(title, newTitle)]
+        for eq in eqs:
+            clearAnim += [FadeOut(eq)]
+        self.play(*clearAnim)
+        self.wait()
+
+        # Clean up are sequence
+        modGroup = VGroup(*modEqs)
+        self.play(modGroup.animate.arrange(RIGHT).shift(LEFT*2.0), FadeOut(mdots))
+        newNums = VGroup(*[MathTex(i) for i in [9, 4, 3, 7]]).arrange(RIGHT).next_to(modGroup, RIGHT)
+        self.play(Write(newNums))
+
 class Pisano(Scene):
     def construct(self):
         title = Text("Pisano Arrays", font_size=bigFont).shift(UP * 3)
@@ -105,7 +123,8 @@ class Pisano(Scene):
             [1, 8, 9, 7, 6, 3, 9, 2, 1, 3, 4, 7],
             [1, 3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2],
             [2, 1, 3, 4, 7, 1, 8, 9, 7, 6, 3, 9],
-            [3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2, 1]])
+            [3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2, 1]
+        ])
 
         pArr.scale(0.75)
 
