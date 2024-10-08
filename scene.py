ade3b832fb345a971fb9907dfce95ba64242236b
Joe Shoulak <joepshoulak@me.com>
2024-10-08 12:35:44 -0500

    polish before big changes

--- scene.py
+++ scene.py
@@ -19,6 +19,12 @@ smallFont = 55
 def alignLines(eq, i):
     return eq.shift(RIGHT * hD * (i+hOff)).shift(DOWN * vD * (i+vOff))
 
+def smallTex(text):
+    return Tex(text, font_size=smallFont)
+
+def writeMany(*mobs):
+    return [Write(i) for i in mobs]
+
 class Fibonacci(Scene):
     def construct(self):
         # Start with title already on screen, and draw the defintition of the Fibonacci numbers
@@ -48,21 +54,23 @@ class Fibonacci(Scene):
         # align all our staggered numbers into one clean row
         alignAnim = list(map(lambda i: eqs[i+1].animate.align_to(eqs[0], UP), range(len(eqs)-1)))
         # add the numbers we lopped off the left, and an elipsis on the right
-        p1 = Tex("1", font_size=smallFont).next_to(eqs[0], LEFT, buff=hD*2/3) # buff is not correct
-        p0 = Tex("0", font_size=smallFont).next_to(p1, LEFT, buff=hD*2/3) # buff is not correct
-        n1 = Tex("13", font_size=smallFont).next_to(eqs[-1], RIGHT, buff=hD*2/3).align_to(p0, UP)
-        n2 = Tex("21", font_size=smallFont).next_to(n1, RIGHT, buff=hD*2/3).align_to(p0, UP)
-        n3 = Tex("34", font_size=smallFont).next_to(n2, RIGHT, buff=hD*2/3).align_to(p0, UP)
-        n4 = Tex("55", font_size=smallFont).next_to(n3, RIGHT, buff=hD*2/3).align_to(p0, UP)
-        cdots = Tex(r"\ldots", font_size=smallFont).next_to(n4, RIGHT, buff=hD*2/3).align_to(p0, UP).shift(DOWN * 0.25) # This is also not correctly aligned
+        p1 = smallTex("1").next_to(eqs[0], LEFT, buff=hD*2/3) # buff is not correct
+        p0 = smallTex("0").next_to(p1, LEFT, buff=hD*2/3) # buff is not correct
+        n1 = smallTex("13").next_to(eqs[-1], RIGHT, buff=hD*2/3).align_to(p0, UP)
+        n2 = smallTex("21").next_to(n1, RIGHT, buff=hD*2/3).align_to(p0, UP)
+        n3 = smallTex("34").next_to(n2, RIGHT, buff=hD*2/3).align_to(p0, UP)
+        n4 = smallTex("55").next_to(n3, RIGHT, buff=hD*2/3).align_to(p0, UP)
+        cdots = smallTex(r"\ldots").next_to(n4, RIGHT, buff=hD*2/3).align_to(p0, UP).shift(DOWN * 0.25) # This is also not correctly aligned
         eqs = [p0, p1] + eqs + [n1, n2, n3, n4]
         self.play(
             AnimationGroup(
                 AnimationGroup(*alignAnim),
-                AnimationGroup(Write(p0), Write(p1), Write(n1), Write(n2), Write(n3), Write(n4), Write(cdots)),
+                AnimationGroup(*writeMany(p0, p1, n1, n2, n3, n4, cdots), FadeOut(definition)),
                 lag_ratio=0.5
             )
         )
+
+        self.wait(1)
 
         # Begin to show all the numbers mod 10
         modEqs = []
@@ -97,20 +105,20 @@ class Fibonacci(Scene):
                     lag_ratio=0.5
                 ))
 
-        self.wait()
+        self.wait(1)
 
         # Psuedo-scene change
         newTitle = Text("Pisano Arrays", font_size=bigFont).to_edge(UP)
-        clearAnim = [FadeOut(definition), FadeOut(cdots), Transform(title, newTitle)]
+        clearAnim = [FadeOut(cdots), Transform(title, newTitle)]
         for eq in eqs:
             clearAnim += [FadeOut(eq)]
         self.play(*clearAnim)
         self.wait()
 
-        # Clean up are sequence
+        # Clean up our sequence
         modGroup = VGroup(*modEqs)
         self.play(modGroup.animate.arrange(RIGHT).shift(LEFT*2.0), FadeOut(mdots))
-        newNums = VGroup(*[MathTex(i) for i in [9, 4, 3, 7]]).arrange(RIGHT).next_to(modGroup, RIGHT)
+        newNums = VGroup(*[MathTex(i) for i in [9, 4, 3, 7, 0, 7, 7, 4, 1, 5, 6, 1, 7, 8, 5, 3, 8, 1, 9, 0, 9, 9, 8, 7, 5, 2, 7, 9, 6, 5, 1, 6, 7, 3, 0, 3, 3, 6, 9, 5, 4, 9, 3, 2, 5, 7, 2, 9, 1]]).arrange(RIGHT).next_to(modGroup, RIGHT)
         self.play(Write(newNums))
 
 class Pisano(Scene):
@@ -118,17 +126,14 @@ class Pisano(Scene):
         title = Text("Pisano Arrays", font_size=bigFont).shift(UP * 3)
         self.add(title)
 
-        pArr = Matrix([
-            [0, 5, 5, 0, 5, 5, 0, 5, 5, 0, 5, 5],
-            [1, 8, 9, 7, 6, 3, 9, 2, 1, 3, 4, 7],
-            [1, 3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2],
-            [2, 1, 3, 4, 7, 1, 8, 9, 7, 6, 3, 9],
-            [3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2, 1]
-        ])
-
-        pArr.scale(0.75)
-
-        self.play(Create(pArr, run_time=3))
+        nums = [0, 1, 1, 2, 3, 5, 8, 3, 1, 4, 5, 9, 4, 3, 7, 0, 7, 7, 4, 1, 5, 6, 1, 7, 8, 5, 3, 8, 1, 9, 0, 9, 9, 8, 7, 5, 2, 7, 9, 6, 5, 1, 6, 7, 3, 0, 3, 3, 6, 9, 5, 4, 9, 3, 2, 5, 7, 2, 9, 1]
+        numMobs = []
+        for i, n in enumerate(nums):
+            numMobs += [smallTex(n)]
+            if i != 0:
+                numMobs[i].next_to(numMobs[i-1], RIGHT)
+            
+        self.add(*numMobs)
         self.wait()
 
 class Test(Scene):
